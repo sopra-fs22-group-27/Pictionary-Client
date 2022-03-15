9cd54461edcdd541b81751bd5a18f8a37cfeabac
radubauzh <rafael.dubach@uzh.ch>
2022-03-15 10:54:18 +0100

    Changed Title

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -13,7 +13,7 @@ import "styles/views/Header.scss";
  */
 const Header = props => (
   <div className="header container" style={{height: props.height}}>
-    <h1 className="header title">SoPra FS22 rocks with React!</h1>
+    <h1 className="header title">Group 27</h1>
     <ReactLogo width="60px" height="60px"/>
   </div>
 );
