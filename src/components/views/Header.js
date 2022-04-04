5d3cdda9dec6734671eca5cc844472a5c0081019
radubauzh <rafael.dubach@uzh.ch>
2022-04-04 11:47:23 +0200

    Made Navbar
    After the last push from shaoyan the navbar was "deleted" and the header wrongly configured. I fixed it.

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {ReactLogo} from "components/ui/ReactLogo";
 import PropTypes from "prop-types";
 import "styles/views/Header.scss";
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import * as FiIcons from 'react-icons/fi';
-// import 'bootstrap/dist/css/bootstrap.css'
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import * as FiIcons from "react-icons/fi";
+import "bootstrap/dist/css/bootstrap.css";
+
 /**
  * This is an example of a Functional and stateless component (View) in React. Functional components are not classes and thus don't handle internal state changes.
  * Conceptually, components are like JavaScript functions. They accept arbitrary inputs (called “props”) and return React elements describing what should appear on the screen.
@@ -13,19 +13,36 @@ import * as FiIcons from 'react-icons/fi';
  * https://reactjs.org/docs/components-and-props.html
  * @FunctionalComponent
  */
-const Header = props => (
-
-
-  <div className="header container">
-    
-    <h1 className="header title">Pictionary</h1>
+const Header = (props) => (
+  <div className="header">
+    <Navbar
+      bg="transparent"
+      variant="dark"
+      sticky="top"
+      expand="lg"
+      collapseOnSelect
+    >
+      <Navbar.Brand>Pictionary</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav className="ms-auto">
+          <Nav.Link href="Change me">Score Board</Nav.Link>
+          <Nav.Link href="Change me">Play</Nav.Link>
+          <Nav.Link href="Change me">Sign out</Nav.Link>
+          <NavDropdown title={<FiIcons.FiSettings color="white" />}>
+            <NavDropdown.Item href="Change me">Edit Profile</NavDropdown.Item>
+            <NavDropdown.Item href="Change me">Edit whatever</NavDropdown.Item>
+            <NavDropdown.Item href="Change me">Sign out </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     {/* <ReactLogo width="60px" height="60px"/> */}
   </div>
-
 );
 
 Header.propTypes = {
-  height: PropTypes.string
+  height: PropTypes.string,
 };
 
 /**
