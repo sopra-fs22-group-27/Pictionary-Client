38b04f498867bfd79fd4f92a66cadbd954f0cebd
radubauzh <rafael.dubach@uzh.ch>
2022-05-07 13:26:00 +0200

    Description

--- src/components/views/Video.js
+++ src/components/views/Video.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 import { useHistory } from "react-router-dom";
 import '../../styles/views/VideoStyles.scss'
 import beachVideo from 'resources/beach.mp4'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Video = (props) => {
     const history = useHistory();
@@ -32,26 +35,40 @@ const Video = (props) => {
             <div className='hero2'>
 
             <div className='content'>
-                <h1>How to play Pictionary</h1>
-                <p>
+            <p></p> {/*sorry lol*/}
+            <h1>How to play Pictionary</h1>
+            <p></p> {/*sorry lol*/}
+
+            <Container>
+            <Row>
+                <Col>Picture</Col>
+                <Col>
+                <text class="text-justify">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lectus enim, vulputate lobortis magna ut, 
                 eleifend tempor dui. Sed orci lorem, dapibus sed dapibus in, euismod vel lacus. Donec placerat tellus elit. 
                 Sed sit amet varius orci, faucibus pellentesque lectus. Vestibulum at felis eu arcu tempus molestie a vel arcu. 
                 Nullam tempus justo nunc, sed iaculis ante interdum sit amet. Nunc in euismod arcu. Mauris arcu lorem, imperdiet 
-                efficitur magna in, efficitur sollicitudin leo. Proin dignissim, nunc ut fermentum aliquam, nisi lorem volutpat 
-                ligula, at lobortis ipsum leo quis magna. Maecenas enim dolor, accumsan at felis elementum, rutrum varius ex. 
-                Aliquam erat volutpat. In ut lectus eu nisl blandit mattis sit amet vel lectus. Morbi quis felis sit amet massa 
-                tristique lobortis quis ac est. Pellentesque sodales fringilla erat sed sodales. Aenean in pharetra velit. 
-                Integer elementum, est ut lobortis finibus, quam risus vehicula urna, eget ultrices purus ante vel urna. 
-                Integer malesuada egestas sapien sed finibus. Mauris scelerisque urna efficitur, interdum felis ac, aliquet erat. 
-                Fusce nec odio auctor, vehicula nunc ac, consectetur urna. In dictum justo non facilisis pellentesque. 
-                Nullam molestie porttitor pellentesque. Ut tristique odio in dolor commodo mattis. Duis consequat ornare purus, 
-                id auctor augue rutrum sodales. Aliquam ultricies, elit et cursus auctor, neque ex tincidunt lacus, eu lacinia 
-                enim ante eget ligula. Nunc maximus lacinia purus ut molestie. Donec pulvinar convallis augue. Quisque vel neque 
-                eu nunc molestie sollicitudin et at orci. In a finibus ligula, quis efficitur odio. Etiam non ligula dolor. 
-                Curabitur at malesuada nibh, non ultricies est. Praesent euismod ligula sed lectus aliquam, a dignissim lorem tempor. 
-                Quisque semper cursus ante sed fringilla.
-                </p>
+                efficitur magna
+                </text>
+                </Col>
+            </Row>
+            </Container>
+            <p></p> {/*sorry lol*/}
+            <Container>
+            <Row>
+                <Col>
+                <text class="text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lectus enim, vulputate lobortis magna ut, 
+                eleifend tempor dui. Sed orci lorem, dapibus sed dapibus in, euismod vel lacus. Donec placerat tellus elit. 
+                Sed sit amet varius orci, faucibus pellentesque lectus. Vestibulum at felis eu arcu tempus molestie a vel arcu. 
+                Nullam tempus justo nunc, sed iaculis ante interdum sit amet. Nunc in euismod arcu. Mauris arcu lorem, imperdiet 
+                efficitur magna
+                </text>
+                </Col>
+                <Col>Picture</Col>
+            </Row>
+            </Container>
+            <Container>Maybe Team here</Container>
             </div>
             </div>
         </div>
