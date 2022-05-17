9836173606436a66f88710435e9d71eea67da172
SyLi9527 <79486258+SyLi9527@users.noreply.github.com>
2022-05-17 23:10:44 +0200

    fix #105 1)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import AppRouter from "components/routing/routers/AppRouter";
 import { useEffect, useState } from "react";
+import { api, handleError } from "helpers/api";
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState(null);
@@ -10,6 +11,26 @@ const App = () => {
     window.addEventListener("storage", () => {
       setCurrentUser(JSON.parse(localStorage.getItem("user")));
     });
+  }, []);
+
+  useEffect(() => {
+    const unloadCallback = async (event) => {
+      event.preventDefault();
+      try {
+        await api.put(`/status/${localStorage.getItem("token")}`);
+        window.localStorage.clear();
+      } catch (error) {
+        alert(
+          `Something went wrong during updating the logged_out status: \n${handleError(
+            error
+          )}`
+        );
+      }
+      
+    };
+
+    window.addEventListener("unload", unloadCallback);
+    return () => window.removeEventListener("unload", unloadCallback);
   }, []);
 
   return (
