d5b1f9cd696b06ff76bea4f7dbd84e69153e84fb
hellasol <hi@solveig.io>
2022-05-24 23:41:39 +0200

    bug fixes

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -23,10 +23,30 @@ var randomPictionaryWords = require('word-pictionary-list');
 const Game = () => {
   const gameToken = window.location.pathname.split("/")[2];
   const userToken = localStorage.getItem("token");
-
+  
   const canvasRef = useRef(null);
-  const ctx = useRef(null);
+  const canvas = () => canvasRef.current;
+  const canvasContext = () => canvas()?.getContext("2d");
+  
+  const [words, setWords] = useState(JSON.parse(localStorage.getItem("words")));
+  const [selectedWord, setSelectedWord] = useState(localStorage.getItem("selectedWord"));
 
+  useEffect(() => {
+    if (selectedWord === null) {
+      localStorage.removeItem('selectedWord');
+    } else {
+      localStorage.setItem('selectedWord', selectedWord);
+    }
+  }, [selectedWord]);
+
+  useEffect(() => {
+    if (words === null) {
+      localStorage.removeItem('words');
+    } else {
+      localStorage.setItem('words', JSON.stringify(words));
+    }
+  }, [words]);
+  
   const [selectedColor, setSelectedColor] = useState("#000000");
   const [selectedWidth, setSelectedWidth] = useState(5);
   const [mouseDown, setMouseDown] = useState(false);
@@ -55,84 +75,57 @@ const Game = () => {
   const [redoArray, setRedoArray] = useState([]);
   const [game, setGame] = useState(null);
   const [gameRound, setGameRound] = useState(null);
-  const [words, setWords] = useState(JSON.parse(localStorage.getItem("words")));
-  const [selectedWord, setSelectedWord] = useState(localStorage.getItem("selectedWord"));
 
-  const isDrawer = !!gameRound && gameRound.drawerToken === userToken;
+  const isLoaded = Boolean(game && gameRound)
+  const isDrawer = gameRound?.drawerToken === userToken;
 
-  useEffect(() => {
-    if (selectedWord === null) {
-      localStorage.removeItem('selectedWord');
-    } else {
-      localStorage.setItem('selectedWord', selectedWord);
-    }
-  }, [selectedWord]);
-
-  useEffect(() => {
-    if (words === null) {
-      localStorage.removeItem('words');
-    } else {
-      localStorage.setItem('words', JSON.stringify(words));
-    }
-  }, [words]);
-
-  const secondsPassed = !gameRound 
+  const secondsPassed = !isLoaded 
     ? null
     : gameRound.roundStartingTime === 0
-    ? null
-    : Math.floor((Date.now() - gameRound.roundStartingTime) / 1000)
-  const secondsRemaining = !!game && !!secondsPassed && Math.max(0, game.roundLength - secondsPassed);
-  const isTicking = secondsRemaining !== null && secondsRemaining !== false;
-  const canDraw = isDrawer && !!selectedWord && secondsRemaining > 0;
+    ? 0
+    : Math.min(game.roundLength, Math.floor((Date.now() - gameRound.roundStartingTime) / 1000));
+
+  const secondsRemaining = isLoaded ? Math.max(0, game.roundLength - secondsPassed) : null;
+  const isTicking = isLoaded ? gameRound.roundStartingTime !== 0 : null;
+  const canDraw = isLoaded ? isDrawer && !!selectedWord && secondsRemaining > 0 : null;
 
   useEffect(() => {
-    if (!isDrawer) return;
-    const wordsStr = localStorage.getItem("words");
-    if (wordsStr) {
-      setWords(JSON.parse(wordsStr));
-      return
+    if (!isLoaded) return;
+    if (!isDrawer) {
+      if (words) {
+        setSelectedWord(null);
+        setWords(null);
+      }
+      return;
     }
+    if (words) return;
     const nrOfWords = 3;
     const newWords = [];
     for (let i = 0; i < nrOfWords; i++) {
       newWords.push( randomPictionaryWords({exactly:1, wordsPerString:1, formatter: (word)=> word.toLowerCase()}));
     }
     setWords(newWords);
-    localStorage.setItem('words', JSON.stringify(newWords));
-  }, [game?.currentGameRound, isDrawer]);
+  }, [isLoaded, isDrawer, words]);
+
+
+  useEffect(async () => {
+    if (!isLoaded) return;
+    if (secondsRemaining === 0) {
+      await finishDrawing();
+    }
+  }, [isLoaded, secondsRemaining])
 
   useEffect(() => {
-    if (isDrawer && gameRound?.roundStartingTime === 0) {
-      setSelectedWord(null)
-    }
-    if (gameRound?.roundStartingTime === 0) {
-      ctx.current.clearRect(0, 0, ctx.current.canvas.width, ctx.current.canvas.height)
-    }
-  }, [isDrawer, gameRound?.roundStartingTime]);
-
-  useEffect(() => {
+    if (!isLoaded) return;
     setOpenModal(isDrawer && !selectedWord);
-  }, [isDrawer, selectedWord]);
+  }, [isLoaded, isDrawer, selectedWord]);
 
   const history = useHistory();
 
   useEffect(async() => {
-    // if (localStorage.getItem("words") !== 'null' && localStorage.getItem("words") !== null && localStorage.getItem("words") !== '[]') {
-    //   console.log("words from local storage");
-    //   setWords(JSON.parse(localStorage.getItem("words")));
-    // } else {
-      // const nrOfWords = 3;
-      // const randomWords = [];
-      // for (let i = 0; i < nrOfWords; i++) {
-      //   randomWords.push( randomPictionaryWords({exactly:1, wordsPerString:1, formatter: (word)=> word.toLowerCase()}));
-      // }
-      // setWords(randomWords);
-      // localStorage.setItem('words', JSON.stringify(words));
-    // } 
-
-    if (canvasRef.current) {
-      ctx.current = canvasRef.current.getContext('2d');
-    }
+    // if (canvasRef.current) {
+    //   ctx.current = canvasRef.current.getContext('2d');
+    // }
 
     const user_score = await api.get("/games/"+gameToken+"/scoreboard")
     var arr = [];
@@ -146,26 +139,23 @@ const Game = () => {
   }, []);
 
   useEffect(() => {
+    if (!isLoaded) return;
     fetchClassification();
-    if (!isDrawer){
+    if (isDrawer){
+      sendImage();
+    } else {
       getImage();
     }
     const interval = setInterval(() => {
       fetchClassification();
-      if (!isDrawer){
+      if (isDrawer){
+        sendImage();
+      } else {
         getImage();
       }
-    }, 1000);
+    }, 500);
     return () => clearInterval(interval);
-  }, [isDrawer]);
-
-  // Always if there is a change in the drawing --> sending image to backend
-  useEffect(() => {
-    if(isDrawer){
-      sendImage();
-    }
-  });
-
+  }, [isLoaded, isDrawer]);
   useEffect(() => {
     updateGame();
     updateGameRound();
@@ -205,8 +195,7 @@ const Game = () => {
   }
 
   const sendImage = async() => {
-    const canvas = document.getElementById("canvas");
-    const img = canvas.toDataURL();
+    const img = canvasRef.current.toDataURL();
 
     const requestBody = JSON.stringify({img});
     try{
@@ -222,11 +211,9 @@ const Game = () => {
     var img = new Image();
     try{
     const img2 = await api.get("games/drawing?gameToken=" + gameToken) 
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext('2d');
     img.onload = function() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(this, 0, 0);
+      canvasContext()?.clearRect(0, 0, canvas()?.width, canvas()?.height);
+      canvasContext()?.drawImage(this, 0, 0);
     }
     img.src = String(img2.data);
     }
@@ -238,19 +225,18 @@ const Game = () => {
 
   const draw = useCallback((x, y) => {
     if (mouseDown && canDraw) {
-      ctx.current.beginPath();
-      ctx.current.strokeStyle = selectedColor;
-      ctx.current.lineWidth = selectedWidth;
-      ctx.current.lineJoin = 'round';
-      ctx.current.moveTo(lastPosition.x, lastPosition.y);
-      ctx.current.lineTo(x, y);
-      ctx.current.closePath();
-      ctx.current.stroke();
+      const ctx = canvasContext();
+      if (!ctx) return;
+      ctx.beginPath();
+      ctx.strokeStyle = selectedColor;
+      ctx.lineWidth = selectedWidth;
+      ctx.lineJoin = 'round';
+      ctx.moveTo(lastPosition.x, lastPosition.y);
+      ctx.lineTo(x, y);
+      ctx.closePath();
+      ctx.stroke();
 
-      setPosition({
-        x,
-        y
-      })
+      setPosition({ x, y })
     }
   }, [lastPosition, mouseDown, selectedColor, setPosition])
 
@@ -259,6 +245,11 @@ const Game = () => {
   }
 
   const finishDrawing = async() => {
+    try{
+        canvasContext()?.clearRect(0, 0, canvas()?.width, canvas()?.height)
+        await sendImage();
+    } catch(error) {
+    }
     try{
       if(game.currentGameRound + 1 === game.numberOfRounds){
         if(isDrawer){
@@ -271,7 +262,7 @@ const Game = () => {
           }
         }
         alert("This game is over");
-        history.push({pathname: "/homepage",});
+        history.push("/homepage");
       }
       // If someone left the game
       else if (game.gameStatus === "finished"){
@@ -301,7 +292,7 @@ const Game = () => {
       setTimeout(() => {
         setIsDeleting("outset")
       }, 100)
-      ctx.current.clearRect(0, 0, ctx.current.canvas.width, ctx.current.canvas.height)
+      canvasContext()?.clearRect(0, 0, canvas()?.width, canvas()?.height)
       setUndoArray([]);
       setUndoArray([]);
       setUndoIndex(-1);
@@ -312,13 +303,13 @@ const Game = () => {
   const erase = () => {
     setIsDrawing("outset")
     setIsErasing("inset")
-    ctx.current.globalCompositeOperation = 'destination-out'
+    canvasContext().globalCompositeOperation = 'destination-out'
   }
 
   const paint = () => {
     setIsDrawing("inset")
     setIsErasing("outset")
-    ctx.current.globalCompositeOperation = 'source-over'
+    canvasContext().globalCompositeOperation = 'source-over'
   }
 
   const onMouseDown = (e) => {
@@ -337,7 +328,7 @@ const Game = () => {
     if (isDrawer){
       setMouseDown(false)
       if (e.type !== 'mouseleave'){
-        setUndoArray([...undoArray, ctx.current.getImageData(0, 0, ctx.current.canvas.width, ctx.current.canvas.height)]);
+        setUndoArray([...undoArray, canvasContext()?.getImageData(0, 0, canvas()?.width, canvas()?.height)]);
         setUndoIndex(undoIndex + 1);
         
       }
@@ -354,11 +345,11 @@ const Game = () => {
           setUndoArray([]);
           setUndoIndex(-1);
           setRedoIndex(redoIndex + 1);
-          ctx.current.clearRect(0, 0, ctx.current.canvas.width, ctx.current.canvas.height)
+          canvasContext()?.clearRect(0, 0, canvas()?.width, canvas()?.height)
         }
       } else{
         setRedoArray([...redoArray, undoArray.pop()]);
-        ctx.current.putImageData(undoArray[undoIndex - 1], 0, 0);
+        canvasContext()?.putImageData(undoArray[undoIndex - 1], 0, 0);
         setUndoIndex(undoIndex - 1);
         setRedoIndex(redoIndex + 1);
       }
@@ -373,7 +364,7 @@ const Game = () => {
     if(isDrawer){
       if(redoIndex >= 0){
         
-        ctx.current.putImageData(redoArray[redoIndex], 0, 0);
+        canvasContext()?.putImageData(redoArray[redoIndex], 0, 0);
         setUndoArray([...undoArray, redoArray.pop()]);
         setUndoIndex(undoIndex + 1);
         setRedoIndex(redoIndex - 1);
@@ -403,7 +394,7 @@ const Game = () => {
   }
 
   const closeWidthPicker = (event, reason) => {
-    if (reason && reason == "backdropClick") 
+    if (reason && reason === "backdropClick") 
         return;
     setOpenWidthPicker(false)
   }
@@ -416,14 +407,7 @@ const Game = () => {
   const pickWord = (word) => async () => {
     await api.put('/games/'+gameToken+"/word/"+word);
     setSelectedWord(word);
-    setWords(null)
   }  
-
-  useEffect(async () => {
-    if (secondsRemaining === 0) {
-      await finishDrawing();
-    }
-  }, [secondsRemaining])
 
   const makeGuess = async(e) =>{
     e.preventDefault();
@@ -482,7 +466,7 @@ const Game = () => {
     </Modal>
 
     <div className="drawing timer">
-       {game && isTicking && <CountdownCircleTimer
+       {isLoaded && isTicking && <CountdownCircleTimer
           size="150"
           strokeWidth="10"
           isPlaying={isTicking}
@@ -558,9 +542,7 @@ const Game = () => {
             onMouseLeave={onMouseUp}
             onMouseMove={onMouseMove}
           />
-       {usernames && isTicking && <div className="drawing chatbox">
-         <Chatbox user={JSON.parse(localStorage.getItem('user'))} usernames={usernames} gameToken={gameToken} />
-       </div>}
+
     </div>
         
      <br />
